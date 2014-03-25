670fdb20c2f8607de610f665a95396a72c1fef7a
ankurkhanna <delkhanna@gmail.com>
2014-03-25 17:35:49 +0100

    Php engine for hotel industries.
    This is a simple php engine to create websites for hotel industries.

---
+++ displayImages.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<?php
+  srand( microtime() * 1000000 );
+  $num = rand( 1, 4 );
+   
+  switch( $num ) 
+  {
+  case 1: $image_file = "/images/alfa.jpg";
+          break;
+  case 2: $image_file = "/images/ferrari.jpg";
+          break;
+  case 3: $image_file = "/images/jaguar.jpg";
+          break;
+  case 4: $image_file = "/images/porsche.jpg";
+          break;
+  }
+  echo "Random Image : <img src=$image_file />";
+?>
+</body>
+</html>
